6995292da9947ec5040e4eb5b03df819593d00f6
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-09-06 12:27:24 -0400

    Basic unit tests.

---
+++ angular-demo/src/app/map/map.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import {MapComponent} from "./map.component";
+import {AppModule} from "../app.module";
+
+describe('MapComponent', () => {
+  let component: MapComponent;
+  let fixture: ComponentFixture<MapComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
