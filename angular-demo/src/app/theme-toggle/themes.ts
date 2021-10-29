ae09a9149cc661600c9d91c84fbc5ca2a4fcf581
Suraj Karambe <bkarambe@gmail.com>
2021-10-29 19:31:03 +0530

    PXBLUE-2527 Reverted the demo projects rebranding

--- angular-demo/src/app/theme-toggle/themes.ts
+++ angular-demo/src/app/theme-toggle/themes.ts
@@ -1,9 +1,9 @@
 export class Theme {
     constructor(public className: string, public themeName: string) {}
 
-    static blueTheme = new Theme('brightlayer-ui-blue', 'Default Theme');
+    static blueTheme = new Theme('pxb-blue', 'Default Theme');
 
-    static darkBlueTheme = new Theme('brightlayer-ui-blue-dark', 'Dark Theme');
+    static darkBlueTheme = new Theme('pxb-blue-dark', 'Dark Theme');
 
     static getThemes(): Theme[] {
         return [Theme.blueTheme, Theme.darkBlueTheme];
